b64002c21b3eecd063c6fbf43fac819f44f77a7c
Ilse Ipsen <ipsen@ncsu.edu>
2015-07-13 14:14:27 -0400

    This is the R file

---
+++ ipsen.R
@@ -0,0 +1,10 @@
+## This is not very interesting
+## draw from 2 distributions
+nn <- 50
+v1 <- rnorm(nn, mean=0, sd=0.1)
+hist(v1, plot = "true", xlab = "Number values in range (a, a +.05)", main = "Histogram")
+
+
+
+
+
